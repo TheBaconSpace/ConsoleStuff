06e7e8e2cd099c5ec86d3efd4d3fecd2cbabc4c0
Bacon_Space <thestreamstats@gmail.com>
2017-01-13 10:10:29 -0500

    Added Version 1.2.0

---
+++ app.js
@@ -0,0 +1,11 @@
+const moment = require('moment');
+const cs = require('./package.json');
+const started = Date()
+const newYork = moment("2014-06-01 12:00", "America/New_York")
+console.log('Started ConsoleStuff' cs.version)
+console.log('Welcome To ConsoleStuff' cs.version)
+console.log('Today is ' + Date() + ' ')
+console.log('Today is ' + newYork.format() + ' ')
+console.log('Xmas is until ' + +moment().date(" 359") + +'')
+console.log('xmas is ' + moment("2018", "YYYYMMDD").fromNow() + '');
+console.log('Ended ConsoleStuff' cs.version)
