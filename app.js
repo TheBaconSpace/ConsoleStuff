ee45957f362b4999d72bb23d7dbb2473cd3c3980
Bacon_Space <thestreamstats@gmail.com>
2017-01-13 10:16:34 -0500

    Fixed a Code Mistake

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 const cs = require('./package.json');
 const started = Date()
 const newYork = moment("2014-06-01 12:00", "America/New_York")
-console.log("Started ConsoleStuff" + cs.version)
+console.log("Started ConsoleStuff Version " + cs.version)
 console.log("Welcome To ConsoleStuff Version " + cs.version)
 console.log("Today is " + Date() + "")
 console.log("Today is " + newYork.format() + "")
